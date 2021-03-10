bb4a141d7111abea89ed0118df1076ce98a87bed
zkolbaeva <zhanara@archis.company>
2021-03-10 11:56:59 +0300

    updated helloWorld

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! has been changed'));
+app.get('/', (req, res) => res.send('Hello World! has been changed AGAIN'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
